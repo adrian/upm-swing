6ece42987643d09f8e96a551d6d3f59a98739245
Adrian Smith <adrian@17od.com>
2007-10-31 18:35:40 +0000

    Added a shell script to start UPM

---
+++ bin/upm.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------
+# $Id:$
+#
+# Universal Password Manager
+# Copyright (C) 2005 Adrian Smith
+#
+# This file is part of Universal Password Manager.
+#
+# Universal Password Manager is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Universal Password Manager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Universal Password Manager; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ----------------------------------------------------------------------------
+
+PRG="$0"
+
+# PRG may be a symbolic link so follow the links to the real program
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG="`dirname "$PRG"`/$link"
+  fi
+done
+
+UPM_HOME=`dirname "$PRG"`
+
+java -jar $UPM_HOME/upm.jar
+
