004fa96a8c4c6c8789f2b763aae4cfedd989f68f
Adrian Smith <adrian@17od.com>
2010-06-22 17:29:21 +0000

    Tidied up the about dialog

--- src/com/_17od/upm/gui/AboutDialog.java
+++ src/com/_17od/upm/gui/AboutDialog.java
@@ -28,54 +28,47 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
 
 import com._17od.upm.util.Translator;
-import com._17od.upm.util.Util;
 
 public class AboutDialog extends EscapeDialog {
 
-    
     public AboutDialog(JFrame frame) {
         super(frame, Translator.translate("aboutUPM"), true);
-        
+
+        String version = AboutDialog.class.getPackage().getImplementationVersion();
+        if (version == null) {
+            version = "<version unknown>";
+        }
+
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
-        panel.add(Box.createRigidArea(new Dimension(0,5)));
+        JTextArea jTextArea = new JTextArea();
+        StringBuffer aboutText = new StringBuffer();
+        aboutText.append("Universal Password Manager\n");
+        aboutText.append(version);
+        aboutText.append("\n\n");
+        aboutText.append("Copyright © 2005-2010 Adrian Smith & Contributors\n\n");
+        aboutText.append("adrian@17od.com\n");
+        aboutText.append("http://upm.sourceforge.net");
+        jTextArea.setText(aboutText.toString());
+        jTextArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jTextArea.setEditable(false);
+        jTextArea.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        jTextArea.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+        panel.add(jTextArea);
+        panel.add(new JSeparator());
 
-        JLabel logo = new JLabel(Util.loadImage("upm.gif"));
-        logo.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(logo);        
-        
-        Font font1 = new Font("Helvetica", Font.PLAIN,  22);
-        JLabel productLabel = new JLabel("UPM");
-        productLabel.setFont(font1);
-        productLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(productLabel);
-        
-        panel.add(Box.createRigidArea(new Dimension(200,10)));
-        
-        Font font2 = new Font("Helvetica", Font.PLAIN,  11);
-        String version = AboutDialog.class.getPackage().getImplementationVersion();
-        JLabel versionLabel = new JLabel(version);
-        versionLabel.setFont(font2);
-        versionLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(versionLabel);
-
-        panel.add(Box.createRigidArea(new Dimension(0,5)));
-
-        JLabel textLabel = new JLabel("http://upm.sourceforge.net");
-        textLabel.setFont(font2);
-        textLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(textLabel);
-
-        panel.add(Box.createRigidArea(new Dimension(0,10)));
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
 
         JButton okButton = new JButton(Translator.translate("ok"));
         okButton.addActionListener(new ActionListener() {
@@ -87,13 +80,12 @@ public class AboutDialog extends EscapeDialog {
         okButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(okButton);
 
-        panel.add(Box.createRigidArea(new Dimension(0,10)));
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
 
         getContentPane().add(panel);
         
         setResizable(false);
         
     }
-
 
 }
