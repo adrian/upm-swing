ac4f8e93c1018289d5da99027c1ddc709b1e0639
bpetewemh <bpetewemh@gmail.com>
2016-02-04 17:22:13 -0500

    Pete fixed his own errors

--- test/PeteTest.java
+++ test/PeteTest.java
@@ -28,23 +28,9 @@ public class PeteTest extends TestCase
         }
     }
 }
+
+
     
     
     
-    /*
-    
-    
-    public void testNonNullDoesntThrowError() {
-        Validator.checkNotNull("I'm not null", errorMsg);
-    }
-    
-    public void testNullThrowsError() {
-        try {
-            Validator.checkNotNull(null, errorMsg);
-            fail("No error was thrown, but expected IllegalArg");
-        } catch (IllegalArgumentException e) {
-            // Do nothing, it was thrown as expected
-        }
-    }
-}
-*/
+  
