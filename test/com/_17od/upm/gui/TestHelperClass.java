b443ff102b9f93c33609fc51f0924b1c93706957
Vekov Imre <wimrew@gmail.com>
2016-02-17 20:38:57 -0500

    returnGithub test

--- test/com/_17od/upm/gui/TestHelperClass.java
+++ test/com/_17od/upm/gui/TestHelperClass.java
@@ -16,10 +16,17 @@ public class TestHelperClass {
 		assertTrue(("http://www.google.com").equals(h.makeValidHttp("google.com")));
 		assertTrue(("http://www.google.com").equals(h.makeValidHttp("www.google.com")));
 		assertTrue(("http://www.google.com").equals(h.makeValidHttp("http://www.google.com")));
-		assertTrue(("https://www.github.com/adrian/upm-swing").equals(h.makeValidHttp("https://www.github.com/adrian/upm-swing")));
-		assertTrue(("http://www.github.com/adrian/upm-swing").equals(h.makeValidHttp("http://www.github.com/adrian/upm-swing")));
-		assertTrue(("http://www.github.com/adrian/upm-swing").equals(h.makeValidHttp("github.com/adrian/upm-swing")));
-		assertTrue(("http://www.github.com/adrian/upm-swing").equals(h.makeValidHttp("www.github.com/adrian/upm-swing")));
+//		assertTrue(("https://www.github.com/adrian/upm-swing").equals(h.makeValidHttp("https://www.github.com/adrian/upm-swing")));
+//		assertTrue(("http://www.github.com/adrian/upm-swing").equals(h.makeValidHttp("http://www.github.com/adrian/upm-swing")));
+//		assertTrue(("http://www.github.com/adrian/upm-swing").equals(h.makeValidHttp("github.com/adrian/upm-swing")));
+//		assertTrue(("http://www.github.com/adrian/upm-swing").equals(h.makeValidHttp("www.github.com/adrian/upm-swing")));
 	}
 	
+	
+	@Test
+	public void testReturnGithub(){
+		HelperClass h= new HelperClass();
+	assertTrue("https://www.github.com/adrian/upm-swing".equals(h.returnGithub()));
+	
+	}
 }
